3cf71be0e54682caa4bfb5daebbba1d87b449113
maiaSinoti <50757381+maiaSinoti@users.noreply.github.com>
2022-08-30 19:44:33 -0300

    Add files via upload
    Programa Aula INF-0990 - Programação em C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var i = 3;           // i é implicitamente do tipo int
+var s = "sausage";   // s é implicitamente do tipo string
+
+var rectMatrix = new int[,]    // rectMatrix é implicitamente do tipo int[,]
+{
+  {0,1,2},
+  {3,4,5},
+  {6,7,8}
+};
+
+var vowels = new[] { 'a', 'e', 'i', 'o', 'u' };   // Compilador infere o tipo char[]
+var x = new[] { 1, 10000000000 };           // Legal - todos os elementos são convertidos para o tipo long
